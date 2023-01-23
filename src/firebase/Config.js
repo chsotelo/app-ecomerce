65111db645181c418fdcbee63b250532d849562c
Mathayus <55334767+chsotelo@users.noreply.github.com>
2023-01-23 17:50:53 -0500

    Add credentianls firebase

--- src/firebase/Config.js
+++ src/firebase/Config.js
@@ -1,9 +1,28 @@
+// export const firebaseConfig = {
+//   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+//   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+//   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.REACT_APP_FIREBASE_APP_ID,
+//   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+// };
+
 export const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  apiKey: 'AIzaSyCe2xHVfnA8QYr-iPikzwWB9bAfxwdHLsU',
+  authDomain: 'beat-ecommerce.firebaseapp.com',
+  projectId: 'beat-ecommerce',
+  storageBucket: 'beat-ecommerce.appspot.com',
+  messagingSenderId: '242716695935',
+  appId: '1:242716695935:web:5a1fb31819187650ea8e4a',
+  measurementId: 'G-DSX8RTX3QD',
 };
+
+// export const firebaseConfig = {
+//   apiKey: 'AIzaSyABNi_1ZW5_Imqim3yx_lI0snQIz0p0i64',
+//   authDomain: 'shop-app-a302e.firebaseapp.com',
+//   projectId: 'shop-app-a302e',
+//   storageBucket: 'shop-app-a302e.appspot.com',
+//   messagingSenderId: '547034577526',
+//   appId: '1:547034577526:web:2297b85f3e6c6f1f537d57',
+// };
